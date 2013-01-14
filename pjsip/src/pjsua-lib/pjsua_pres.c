1c7a6e81603cecee7efa6096625f315dee8976ef
Maxim Kondratenko <Maxim.Kondratenko@portaone.com>
2013-01-14 14:54:23 +0200

    mark dialog state as "NULL" if there are info available

--- pjsip/src/pjsua-lib/pjsua_pres.c
+++ pjsip/src/pjsua-lib/pjsua_pres.c
@@ -348,11 +348,17 @@ PJ_DEF(pj_status_t) pjsua_buddy_get_blf_info( pjsua_buddy_id buddy_id,
     pj_strncpy(&info->uri, &buddy->uri, sizeof(info->buf_)-total);
     total += info->uri.slen;
 
-    if (pjsua_var.buddy[buddy_id].blf_status.info[0].dialog_node) {
+    if (pjsua_var.buddy[buddy_id].blf_status.info[0].dialog_info_entity.slen > 0) {
         info->dialog_info_entity.ptr = info->buf_ + total;
         pj_strncpy(&info->dialog_info_entity, &pjsua_var.buddy[buddy_id].blf_status.info[0].dialog_info_entity, pjsua_var.buddy[buddy_id].blf_status.info[0].dialog_info_entity.slen);
         total += info->dialog_info_entity.slen;
-
+    }
+    if (pjsua_var.buddy[buddy_id].blf_status.info[0].dialog_state.slen == 0) {
+        info->dialog_state.ptr = info->buf_ + total;
+        info->dialog_state = pj_str("NULL");
+        total += info->dialog_state.slen;
+    }
+    if (pjsua_var.buddy[buddy_id].blf_status.info[0].dialog_node) {
         info->dialog_id.ptr = info->buf_ + total;
         pj_strncpy(&info->dialog_id, &pjsua_var.buddy[buddy_id].blf_status.info[0].dialog_id, sizeof(info->buf_)-total);
         total += info->dialog_id.slen;
